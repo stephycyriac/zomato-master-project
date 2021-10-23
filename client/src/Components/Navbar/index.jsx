7fe8114a09924a8457ec463691bbdcd757e10ad2
stephy cyriac <cyriacstephy@gmail.com>
2021-10-23 12:04:39 +0530

    fixed navbar width issue

--- client/src/Components/Navbar/index.jsx
+++ client/src/Components/Navbar/index.jsx
@@ -6,7 +6,7 @@ import {BsSearch} from "react-icons/bs";
 //mobile Nav
 const MobileNav = () => {
 return(
-<div className="flex w-full items-center justify-between md:hidden"> 
+<div className="flex w-full items-center justify-between lg:hidden"> 
 <div className="w-24 ">
     <img src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png"
          alt ="logo"  className="w-full h-full"
@@ -25,15 +25,16 @@ return(
 };
 
 //Medium nav
-const MediumNav = () => {
+const LargeNav = () => {
 return(
     <>
- <div className=" hidden w-full items-center md:flex gap-3">
-    <div className="w-24">
+    <div className="container px-20 mx-auto">
+ <div className=" hidden w-full items-center lg:flex justify-between gap-3">
+    <div className="w-32">
     <img src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png"
          alt ="logo"  className="w-full h-full"/>
     </div> 
-    <div className="w-full bg-white shadow-md p-3 border border-gray-200 rounded-md flex items-center gap-3">
+    <div className="w-3/4 bg-white shadow-md p-3 border border-gray-200 rounded-md flex items-center gap-3">
         <div className="flex items-center gap-2 border-r-2 border-gray-300 pr-3"> 
         <span className="text-zomato-300"><HiLocationMarker/></span>
         <input type="text" placeholder="Kochi" className="focus:outline-none"/>
@@ -45,10 +46,11 @@ return(
             <input type="search" placeholder="Search for restaurant,cuisine or a dish" className="focus:outline-none w-full"/>
         </div>
     </div>
-    <div className="flex gap-3">
+    <div className=" ml-20 flex gap-4">
         <button className="text-gray-500 text-xl hover:text-gray-700 ">Login</button>
         <button className="text-gray-500 text-xl hover:text-gray-700">Signup</button>
     </div>
+ </div>
  </div>
     </>
 );
@@ -62,7 +64,7 @@ return (
 <>
 <nav className="p-4 flex bg-white items-center w-full shadow-md">
     <MobileNav/>
-    <MediumNav/>
+    <LargeNav/>
 </nav>
 </>
 );
