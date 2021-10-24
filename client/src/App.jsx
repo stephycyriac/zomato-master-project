69aad04c0a5b57398cfd1b12c541c219e939c83d
stephy cyriac <cyriacstephy@gmail.com>
2021-10-24 12:42:58 +0530

    added active tab for mobile screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,13 @@ import HomeLayoutHOC from "./HOC/Home.Hoc";
 
 //Components
 import Temp from "./Components/temp";
+import Master from "./Components/master";
+
 function App() {
   return (
  <>
  <HomeLayoutHOC path="/" exact component={Temp}/>
+ <HomeLayoutHOC path="/:type" exact component={Master}/>
  </>
   );
 }
--- client/src/Components/FoodTab/index.jsx
+++ client/src/Components/FoodTab/index.jsx
@@ -1,30 +1,59 @@
-import React from 'react';
+import React ,{useState, useEffect }from 'react';
+import { useParams } from 'react-router';
+import { Link } from 'react-router-dom'
 import {FiShoppingBag} from "react-icons/fi";
 import {RiFootprintLine} from "react-icons/ri";
 import {BiDrink} from "react-icons/bi";
 import {IoNutritionOutline} from "react-icons/io5";
 
 
+
 const MobileTab = () => {
+    const [allTypes , setAllTypes] = useState([
+     {
+         id:`delivery`,
+         icon :<FiShoppingBag/>,
+         name:"Delivery",
+         isActive:false,
+     },
+     {
+        id:`dining`,
+        icon :<RiFootprintLine/>,
+        name:"Dining Out",
+        isActive:false,
+    },
+    {
+        id:`night`,
+        icon :<BiDrink/>,
+        name:"Night Life",
+        isActive:false,
+    },
+    {
+        id:`nutrition`,
+        icon :<IoNutritionOutline/>,
+        name:"Nutrition",
+        isActive:false,
+    },
+    ]);
+    const {type} = useParams();
+
+  
  return(
 <> 
 <div className="lg:hidden bg-white shadow-lg p-3 fixed bottom-0 z-10  w-full flex items-center justify-between md:justify-evenly text-gray-500 border">
-<div className="flex flex-col text-xl items-center">
-<FiShoppingBag/>
-<h5 className="text-sm">Delivery</h5>
+{
+    allTypes.map((items) => (
+        <Link to ={`${items.id}`}>
+<div className={ 
+     type=== items.id
+                 ? "flex flex-col relative text-xl items-center text-zomato-400" 
+                 :  "flex flex-col text-xl items-center "       }>
+    <div className={type=== items.id && "absolute  -top-3 w-full h-2 border-t-2 border-zomato-400"}/>                                   
+{items.icon}
+<h5 className="text-sm">{items.name}</h5>
 </div>
-<div className="flex flex-col text-xl items-center">
-<RiFootprintLine/>
-<h5 className="text-sm">Dining Out</h5>
-</div>
-<div className="flex flex-col text-xl items-center">
-<BiDrink/>
-<h5 className="text-sm">Night Life</h5>
-</div>
-<div className="flex flex-col text-xl items-center">
-<IoNutritionOutline/>
-<h5 className="text-sm">Nutrition</h5>
-</div>
+</Link>
+    ))}
 
 </div>
 </>
