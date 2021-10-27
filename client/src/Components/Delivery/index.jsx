ee4311ac89678fcd9fd92baf966367d546a715bc
stephy cyriac <cyriacstephy@gmail.com>
2021-10-27 21:30:49 +0530

    added restaurant card in delivery section

--- client/src/Components/Delivery/index.jsx
+++ client/src/Components/Delivery/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 //components
 import DeliveryCarousel from './DeliveryCarousel';
@@ -7,11 +7,48 @@ import RestaurantCard from '../RestaurantCard';
 
 
 const Delivery = () => {
+    const [restaurantList , setRestaurantList ] = useState([
+        {
+            _id:"12345",
+            photos:["https://b.zmtcdn.com/data/pictures/1/2900231/1588ab7f544d4433e6f476a7469e217e.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"],
+            name:"Domino's Pizza",
+            cuisine:["Pizza" ,"Fast Food"],
+            averageCost:200,
+            isPro:true,
+            isOff:80,
+            RestaurantreviewValue:4.4,
+            durationOfDelivery:"30min",
+        },
+        {
+            _id:"5785",
+            photos:["https://b.zmtcdn.com/data/pictures/7/18752947/fa116d1ea821dbf2d1870a4a64b38886.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"],
+            name:"Manomay",
+            cuisine:["Mithai", "Chinese", "Street Food", "North Indian", "Fast Food", "South Indian"],
+            averageCost:150,
+            RestaurantreviewValue:4.2,
+            durationOfDelivery:"32min",
+        },
+        {
+            _id:"987654",
+            photos:["https://b.zmtcdn.com/data/pictures/5/2900405/3218168b9e8dddebe3f1dad5a6e89b20.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"],
+            name:"Green Chillyz",
+            cuisine:["Biryani", "Chinese", "Kebab"],
+            averageCost:150,
+            isPro:true,
+            isOff:60,
+            RestaurantreviewValue:4.0,
+            durationOfDelivery:"25min",
+        },
+    ]);
     return (
         <> 
         <DeliveryCarousel/>
         {/* <Brand/> */}
-        <RestaurantCard/>
+       <div className="flex flex-wrap justify-between">
+     {restaurantList.map((restaurant) => (
+         <RestaurantCard {...restaurant} key={restaurant._id}/>
+     ))}
+       </div>
         </>
     );
 };
--- client/src/Components/RestaurantCard.jsx
+++ client/src/Components/RestaurantCard.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import {AiTwotoneStar} from "react-icons/ai";
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
     return (
         <>
-            <div className="bg-white p-2 w-full mb-20 rounded-2xl hover:shadow-lg transition duration-700 ease-in-outgit  md:w-1/2 lg:w-1/3">
+            <div className="bg-white p-4 w-full mb-4 rounded-2xl hover:shadow-lg transition duration-700 ease-in-outgit   md:w-1/2 lg:w-1/3">
                   <div className="w-full h-56 lg:h-64 relative ">
                      <div className="absolute flex items-end w-full bottom-3 justify-between">
                      <div className="flex flex-col gap-3 items-start">
-                     <span className="bg-pink-600 text-white px-2 text-sm">Pro extra 10% OFF</span>
-                      <span className="bg-blue-500 text-white px-2 text-sm"> 30% OFF</span>
+                   { props.isPro && ( <span className="bg-pink-600 text-white px-2 text-sm">Pro extra 10% OFF</span>) }
+                    { props.isPro && (  <span className="bg-blue-500 text-white px-2 text-sm">₹{`${props.isOff}`} OFF</span>) }
                      </div>
-                      <span className="bg-white bg-opacity-75 rounded-md p-1 mr-3 text-sm">29 min</span>
+                      <span className="bg-white bg-opacity-75 rounded-md p-1 mr-3 text-sm">{props.durationOfDelivery}</span>
                      </div>
-                      <img src="https://b.zmtcdn.com/data/pictures/0/19650970/7c99e430727a823c8f8acf03b3c04978_o2_featured_v2.jpg"
+                      <img src={props.photos.length && props.photos[0]}
                       alt=" food" className="w-full h-full rounded-2xl"/>
 
                   </div>
                   <div classname="flex flex-col gap-3">
                       <div className="flex  items-center py-3 justify-between">
-                          <h4 className="text-xl font-medium"> Akbar Fast Food </h4>
-                          <span className="bg-green-600 text-white rounded-md p-1 mr-3 text-sm flex items-center ">3.9<AiTwotoneStar/></span>
+                          <h4 className="text-xl font-medium">{props.name}</h4>
+                          <span className="bg-green-600 text-white rounded-md p-1 mr-3 text-sm flex items-center ">{props.RestaurantreviewValue}<AiTwotoneStar/></span>
                       </div>
                       <div className="flex items-center justify-between text-sm text-gray-500">
-                          <p>South Indian </p>
-                          <p>₹200 for one  </p>
+                          <p>{props.cuisine.join(", ")}</p>
+                          <p>₹{props.averageCost} for one  </p>
                       </div>
 
                   </div>
