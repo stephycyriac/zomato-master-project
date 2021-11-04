c34d91b879cbfd6e2a5bbc02b0cecf40aa95e39b
stephy cyriac <cyriacstephy@gmail.com>
2021-11-04 21:00:06 +0530

    added order-online side bar

--- client/src/Page/RestaurantTabs/OrderOnline.jsx
+++ client/src/Page/RestaurantTabs/OrderOnline.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
 
-
 //components
-import FloatMenuBtn from '../../Components/Restaurant/Order-Online/FloatMenuBtn'
+import FloatMenuBtn from '../../Components/Restaurant/Order-Online/FloatMenuBtn';
+import MenuListContainer from '../../Components/Restaurant/Order-Online/MenuListContainer';
+
 const OrderOnline = () => {
     return (
         <>
             <div className="w-full">
-                <FloatMenuBtn/>
-
+                <aside className="hidden md:flex flex-col border-r border-gray-200 overflow-y-scroll h-screen gap-3 w-1/4">
+                <MenuListContainer/>
+                <MenuListContainer/>
+                <MenuListContainer/>
+                <MenuListContainer/>
+                <MenuListContainer/>
+                <MenuListContainer/>
+                <MenuListContainer/>
+                <MenuListContainer/>
+                <MenuListContainer/>
+                <MenuListContainer/>
+                <MenuListContainer/>
+                <MenuListContainer/>
+                <MenuListContainer/>
+                <MenuListContainer/>
+                <MenuListContainer/>
+                <MenuListContainer/>
+                </aside>
+                <div className="w-full md:w-3/4"></div>
             </div>
+            <FloatMenuBtn/>
+
         </>
     )
 }
